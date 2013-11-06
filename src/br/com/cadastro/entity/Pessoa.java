15155541f052512cb3057192763983ebb8ca2798
RodrigoBill <ramosrodrigo@gmail.com>
2013-11-06 15:24:55 -0200

    Primeiro commit cadastro

---
+++ src/br/com/cadastro/entity/Pessoa.java
@@ -0,0 +1,152 @@
+package br.com.cadastro.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Generated(value = { "pessoa" })
+@ManagedBean
+public class Pessoa implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column
+	private String nome;
+	private String email;
+	private String responsavel;
+	private String endereco;
+	private String facebook;
+	private Integer telefone;
+	private Integer idade;
+	private String igreja;
+	private String batismo;
+	private String atividade;
+	private String observacao;
+	private String sugestao;
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getFacebook() {
+		return facebook;
+	}
+
+	public void setFacebook(String facebook) {
+		this.facebook = facebook;
+	}
+
+	public Integer getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Integer telefone) {
+		this.telefone = telefone;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getIgreja() {
+		return igreja;
+	}
+
+	public void setIgreja(String igreja) {
+		this.igreja = igreja;
+	}
+
+	public String getBatismo() {
+		return batismo;
+	}
+
+	public void setBatismo(String batismo) {
+		this.batismo = batismo;
+	}
+
+	public String getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(String atividade) {
+		this.atividade = atividade;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public String getSugestao() {
+		return sugestao;
+	}
+
+	public void setSugestao(String sugestao) {
+		this.sugestao = sugestao;
+	}
+
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
